501109a838da6cd5ad68ad6e2d80cad4a6067f5e
motvey282 <56763278+motvey282@users.noreply.github.com>
2022-10-04 05:10:52 +0300

    Update test_items.py
    add time.sleep

--- test_items.py
+++ test_items.py
@@ -2,12 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+import time
 
 
 
 def test_items(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     browser.get(link)
+    time.sleep(20)
     try:
         browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
     except (NoSuchElementException):
